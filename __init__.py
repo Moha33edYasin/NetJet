1c013ddcb8fafe0faa9c7cdbc93953dfbd06bece
Mohammed Yasin <mhsingo.shift17@gmail.com>
2026-05-13 18:20:46 +0400

    Added validation message

--- __init__.py
+++ __init__.py
@@ -2,4 +2,8 @@ __all__ = ("models", "methods", "Layers")
 
 from models import * 
 from methods import * 
-from Layers import * 
+from Layers import *
+
+message = 'NetJet v1.0.0 is successfully working'
+
+print(message)
